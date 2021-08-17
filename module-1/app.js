dc59d6e6a860ae5a61f4c7cdc0a5e25767aed243
Chandrika Dalakoti <70463605+chandrikadalakoti2@users.noreply.github.com>
2021-08-17 16:23:04 +0530

    Add files via upload

---
+++ module-1/app.js
@@ -0,0 +1,34 @@
+(function()
+{
+    'use strict';
+
+    angular.module('LunchCheck',[])
+    .controller('LunchCheckController',function($scope)
+    {
+        var arr=[],c=0;
+        $scope.Check_If_Too_Much=function()
+        {
+          if($scope.name==undefined)
+          $scope.value='Please enter data first';
+          else{
+          arr=$scope.name.split(',');
+          for(var i=0;i<arr.length;i++)
+          {
+              c=c+1;
+          }
+          if(c<=3)
+          {
+            $scope.value='Enjoy!';
+            c=0;
+          }
+          else
+          {
+          $scope.value='Too much!';
+          c=0;
+        }
+    }
+        };
+
+    });
+
+})();
